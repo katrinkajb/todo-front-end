c6a3f55e303f1b6809fc5199f718100d5e8cca29
KB <katrinkajb@gmail.com>
2021-03-02 15:35:14 -0800

    started each file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Header from './components/Header.js';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Header />
+            <Switch>
+              <Route 
+                path="/" 
+                exact
+                render={(routerProps) => <Home {...routerProps} />} 
+              />
+              <Route 
+                path="/signup" 
+                exact
+                render={(routerProps) => <SignUpPage {...routerProps} />} 
+              />
+              <Route 
+                path="/login" 
+                exact
+                render={(routerProps) => <LoginPage {...routerProps} />} 
+              />
+              <Route 
+                path="/todos" 
+                exact
+                render={(routerProps) => <ToDoPage {...routerProps} />} 
+              />
+            </Switch>
+          </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
